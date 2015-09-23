aae2a0ad200fceedcc8dd74285f48b0fc4e08e80
Chris Snyder <basilard99@gmail.com>
2015-09-22 17:57:06 -0700

    removed hard-coded path to index.html

--- tests/e2e/spec/main.spec.js
+++ tests/e2e/spec/main.spec.js
@@ -1,13 +1,23 @@
 describe('Main Page', function() {
+	// Replace backslashes with forward slashes. On a Windows machine
+	// this will convert to a url style. If not, then it won't do
+	// anything. (I think)
+	var parts = __dirname.replace(/\\/g, '/').split('/');
+	
+	// Now that we have the parts, put them back together
+	var localPath = 'file:///';	
+	for (var i = 0; i < parts.length - 3; i++) { localPath = localPath + parts[i] +'/'; } 
+	
     it('should redirect index.html to index.html#/main', function() {
-        browser.driver.get('file:///C:/Users/Chris/Development/BIClient/index.html#/');
-         browser.getLocationAbsUrl().then(function(url) {
-            expect(url).toEqual('/main');
+		
+		browser.driver.get(localPath + 'index.html#/');
+        browser.getLocationAbsUrl().then(function(url) {
+			expect(url).toEqual('/main');
         });
     });
 
     it('should redirect garbage request to index.html/main', function() {
-        browser.driver.get('file:///C:/Users/Chris/Development/BIClient/index.html#/garbage');
+        browser.driver.get(localPath + 'index.html#/garbage');
         browser.getLocationAbsUrl().then(function(url) {
             expect(url).toEqual('/main');
         });
